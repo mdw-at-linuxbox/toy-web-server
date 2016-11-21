edb02a4a0ddfe0983318c0a7eee1b0fe6b85b083
Marcus Watts <mwatts@redhat.com>
2016-11-21 09:56:08 -0500

    Actually wait to service requests, show results (works, trivial case, no real pages)

--- m.c
+++ m.c
@@ -1,7 +1,10 @@
 #include <pthread.h>
-#include "civetweb.h"
 #include <stdlib.h>
+#include <unistd.h>
 #include <stdio.h>
+#include <string.h>
+
+#include "civetweb.h"
 
 struct myhttpd_data {
 	int dummy;
@@ -12,6 +15,42 @@ int rc;
 int
 my_begin_request(struct mg_connection *conn)
 {
+	struct mg_request_info const *req_info = mg_get_request_info(conn);
+	struct myhttpd_data *me = (struct myhttpd_data*)(req_info->user_data);
+	int i;
+	char const *cl = 0;
+
+	fprintf (stdout, "method: %s\n", req_info->request_method);
+	fprintf (stdout, "uri: %s\n", req_info->uri);
+	if (req_info->query_string)
+		fprintf (stdout, "qs: %s\n", req_info->query_string);
+	fprintf (stdout, "user: %s\n", req_info->remote_user);
+	for (i = 0; i < req_info->num_headers; ++i) {
+		if (!strncasecmp("content-length",
+				req_info->http_headers[i].name, 14))
+			cl = req_info->http_headers[i].value;
+		fprintf (stdout, "hd%d: %s=%s\n", i,
+			req_info->http_headers[i].name,
+			req_info->http_headers[i].value);
+	}
+	if (!strcmp(req_info->request_method, "POST")) {
+		char buf[1024];
+		char *ep = 0;
+		int c, totlen, sofar;
+		totlen = cl ? strtol(cl, &ep, 0) : -1;
+		if (cl)
+			fprintf(stdout, "reading content: %d\n", totlen);
+		for (sofar = 0;;sofar += c) {
+			if (totlen >= 0 && sofar >= totlen) break;
+			c = totlen - sofar;
+			if (c > sizeof buf) c = sizeof buf;
+			c = mg_read(conn, buf, c);
+			if (c <= 0) break;
+			fprintf (stdout, "dt%d-%d: %.*s\n",
+				sofar, sofar+c, c, buf);
+		}
+	}
+	return 0;
 }
 
 int
@@ -39,8 +78,9 @@ main(int ac, char **av)
 {
 	struct mg_context *ctx;
 	struct myhttpd_data ud[1];
-	char *options[8], **cpp;
+	char *options[40], **cpp;
 
+	cpp = options;
 	*cpp++ = "decode_url", *cpp++ = "no";
 	*cpp++ = "enable_keep_alive", *cpp++ = "yes";
 	*cpp++ = "validate_http_method", *cpp++ = "no";
@@ -48,5 +88,8 @@ main(int ac, char **av)
 	*cpp++ = "listening_ports", *cpp++ = "5080";
 
 	ctx = mg_start(cb, ud, (const char **) options);
+	for (;;) {
+		pause();
+	}
 	exit(rc);
 }
